a25a98c2386dc82a7dcbc3e7799cd3b90f05c227
Adrian Bezerra Pires Martins <adrian.martins@cnj.jus.br>
2025-11-06 09:22:10 -0300

    feat: add middleware readReqBody to read the request body

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,23 @@
+const tasks = []
+
 export const routes = [
   {
     method: "GET",
     path: "/tasks",
     handler: (req, res) => {
-      return res.end("Hello world!")
+
+      return res.end(JSON.stringify(tasks))
     }
-  }
+  },
+  {
+    method: "POST",
+    path: "/tasks",
+    handler: (req, res) => {
+      const { newTask } = req.body
+
+      tasks.push(newTask)
+
+      return res.end("Tarefa adicionada com sucesso!")
+    }
+  },
 ]
--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 import http from "node:http"
 import { routes } from "./routes.js"
+import { readReqBody } from "./middleware/read-req-body.js"
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req
+
+  await readReqBody(req, res)
 
   const route = routes.find((route) => {
     return route.method === method && route.path === url
