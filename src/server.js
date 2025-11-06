d9c5d132b4e3e3e985758ce497290b096190967e
Adrian Bezerra Pires Martins <adrian.martins@cnj.jus.br>
2025-11-06 09:07:40 -0300

    feat: add routes in a separate file to handle HTTP requests

--- src/server.js
+++ src/server.js
@@ -1,6 +1,18 @@
 import http from "node:http"
+import { routes } from "./routes.js"
 
 const server = http.createServer((req, res) => {
+  const { method, url } = req
+
+  const route = routes.find((route) => {
+    return route.method === method && route.path === url
+  })
+
+  if(route) {
+    return route.handler(req, res)
+  }
+
+
   return res
     .writeHead(404)
     .end()
